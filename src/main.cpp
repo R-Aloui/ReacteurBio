8e9c48b7e9ec899abe8ed8c1e94fb2c836b02531
R. ALOUI <aloui.radhouane@gmail.com>
2022-08-10 00:49:58 +0100

    Use specific offeset for each sensor

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 #include "common.hpp"
 
 MY_SEN0257 sensors[] = {MY_SEN0257(A0), MY_SEN0257(A1), MY_SEN0257(A2), MY_SEN0257(A3)};
+// These values have been taken from the calibration results of each sensor.
+float offesets[] = {0.459, 0.479, 0.469, 0.459};
 int nbLect = 0;
 
 void setup(){
@@ -11,7 +13,7 @@ void setup(){
   // Calibration according to previous experiments
   for (size_t i = 0; i < 4; i++)
   {
-    sensors[i].setOffeset(0.474);
+    sensors[i].setOffeset(offesets[i]);
   }
   MY_PRINT("Starting readings ...\n");
   MY_PRINT("nb,\tp0,\tp1,\tp2,\tp3,\tdp0,\tdp1\n");
@@ -25,6 +27,6 @@ void loop(){
     p[i] = sensors[i].getPressure();
   }
   MY_PRINT("%5d,%10.3f,%10.3f,%10.3f,%10.3f,%10.3f,%10.3f\n", nbLect++, 
-      p[0], p[1], p[2], p[3], p[0] - p[1], p[2] - p[3]);
-  delay(500);
+      p[0], p[1], p[2], p[3], p[1] - p[0], p[3] - p[2]);
+  delay(1000);
 }
